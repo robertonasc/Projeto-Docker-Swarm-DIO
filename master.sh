cabab1c95c5e8742d705282e987d8b6fd539db3a
robertonasc <125284167+robertonasc@users.noreply.github.com>
2025-02-13 16:59:51 -0300

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker swarm init --advertise-addr=10.10.10.100
+sudo docker swarm init --advertise-addr=10.0.10.10
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
