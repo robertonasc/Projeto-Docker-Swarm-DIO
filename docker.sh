e729a76759e024e32048194bc825a0885ca461db
robertonasc <125284167+robertonasc@users.noreply.github.com>
2025-02-13 16:58:37 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,1 +1,5 @@
-# Projeto-Docker-Swarm-DIO
+#!/bin/bash
+curl -fsSL https://get.docker.com | sudo bash
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
